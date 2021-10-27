14a9137fa89a5ca0de2956de9ece907c18b27428
Badgerinator <badgerinator.can@gmail.com>
2021-10-27 23:48:56 +0300

    ödev bitirildi
    backward el. kullanılarak p-val optimize edildi

--- multiple LR/ödev1/odev_cozum_hazirlik.py
+++ multiple LR/ödev1/odev_cozum_hazirlik.py
@@ -21,22 +21,45 @@ veriler = pd.read_csv('odev_tenis.csv')
 #veri on isleme
 
 #encoder:  Kategorik -> Numeric
+play=veriler.iloc[:,-1:].values
 from sklearn.preprocessing import LabelEncoder
-veriler2 = veriler.apply(LabelEncoder().fit_transform)
+
+
+le = LabelEncoder()
+
+
+#labelencoder ile yes/no 'lar 1ve0'lara dönüştü
+play[:,-1] = le.fit_transform(veriler.iloc[:,-1])
+
+
+
+#le ile windy 1-0
+
+windy = veriler.iloc[:,-2:-1].values
+
+windy[:,-1] = le.fit_transform(veriler.iloc[:,-1])
+
+
+### important encoding trick!!!!
+
+from sklearn import preprocessing
+veriler2 = veriler.apply(preprocessing.LabelEncoder().fit_transform)
+# kategorik değerleri kısa yoldan hallettik Fakat;
+# numerik değerler ve ordinal olmayan outlook column da encode edildi bu yüzden;
 
 c = veriler2.iloc[:,:1]
 from sklearn.preprocessing import OneHotEncoder
-ohe = OneHotEncoder(categorical_features='all')
+ohe = OneHotEncoder()
 c=ohe.fit_transform(c).toarray()
 print(c)
 
-havadurumu = pd.DataFrame(data = c, index = range(14), columns=['o','r','s'])
+havadurumu = pd.DataFrame(data = c, index = range(14), columns=['o','r','s'],)
 sonveriler = pd.concat([havadurumu,veriler.iloc[:,1:3]],axis = 1)
 sonveriler = pd.concat([veriler2.iloc[:,-2:],sonveriler], axis = 1)
-
+print(sonveriler)
 
 #verilerin egitim ve test icin bolunmesi
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 x_train, x_test,y_train,y_test = train_test_split(sonveriler.iloc[:,:-1],sonveriler.iloc[:,-1:],test_size=0.33, random_state=0)
 
 from sklearn.linear_model import LinearRegression
@@ -48,16 +71,26 @@ y_pred = regressor.predict(x_test)
 
 print(y_pred)
 
-import statsmodels.formula.api as sm 
+
+### BACKWARD ELIMINATION TIMEEEE
+
+
+
+import statsmodels.api as sm 
 X = np.append(arr = np.ones((14,1)).astype(int), values=sonveriler.iloc[:,:-1], axis=1 )
+
+
 X_l = sonveriler.iloc[:,[0,1,2,3,4,5]].values
+X_l = np.array(X_l,dtype=float)
 r_ols = sm.OLS(endog = sonveriler.iloc[:,-1:], exog =X_l)
 r = r_ols.fit()
 print(r.summary())
 
+
+# p-val yüksek x1 column silindi(windy)
 sonveriler = sonveriler.iloc[:,1:]
 
-import statsmodels.formula.api as sm 
+import statsmodels.api as sm 
 X = np.append(arr = np.ones((14,1)).astype(int), values=sonveriler.iloc[:,:-1], axis=1 )
 X_l = sonveriler.iloc[:,[0,1,2,3,4]].values
 r_ols = sm.OLS(endog = sonveriler.iloc[:,-1:], exog =X_l)
